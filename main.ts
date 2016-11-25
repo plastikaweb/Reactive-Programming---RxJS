94730c6061b472881661ccaac9727c490b9fd612
Plastikaweb <info@plastikaweb.com>
2016-11-25 09:57:19 +0100

    easier observer

--- main.ts
+++ main.ts
@@ -3,17 +3,8 @@ import {Observable, Observer} from 'rxjs';
 let numbers = [1, 3, 23];
 let source = Observable.from(numbers);
 
-class MyObserver implements Observer<number> {
-    next(value) {
-        console.log('value', value);
-    }
-
-    error(e) {
-        console.log('error', e);
-    }
-
-    complete() {
-        console.log('complete');
-    }
-}
-source.subscribe(new MyObserver());
+source.subscribe(
+    value => console.log('value', value),
+    e => console.log('error', e),
+    () => console.log('complete')
+);
