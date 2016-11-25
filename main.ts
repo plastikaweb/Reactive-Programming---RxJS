55bcbe0494cfce6ea597dfddb47d845f56414ff6
Plastikaweb <info@plastikaweb.com>
2016-11-25 09:54:08 +0100

    first observable example

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-alert('ok!!!');
+import {Observable} from 'rxjs';
+
+let numbers = [1, 3, 23];
+let source = Observable.from(numbers);
+
+class MyObserver {
+    next(value) {
+        console.log('value', value);
+    }
+
+    error(e) {
+        console.log('error', e);
+    }
+
+    complete() {
+        console.log('complete');
+    }
+}
+source.subscribe(new MyObserver());
