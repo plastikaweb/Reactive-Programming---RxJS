2b90d51e429b23a4d647711a2454c468631b1a8e
Plastikaweb <info@plastikaweb.com>
2016-11-29 11:09:09 +0100

    Retry Logic with Retry and RetryWhen

--- main.ts
+++ main.ts
@@ -10,15 +10,35 @@ function load(url: string) {
         let xhr = new XMLHttpRequest();
 
         xhr.addEventListener('load', () => {
-            let data = JSON.parse(xhr.responseText);
-            observer.next(data);
-            observer.complete();
+            if (xhr.status === 200) {
+                let data = JSON.parse(xhr.responseText);
+                observer.next(data);
+                observer.complete();
+            } else {
+                observer.error(xhr.statusText);
+            }
+
         });
 
         xhr.open('GET', url);
         xhr.send();
-    })
+    }).retryWhen(retryStrategy({
+        attempts: 3,
+        delay: 1500
+    }));
 
+}
+
+function retryStrategy({attempts = 4, delay = 1000}) {
+    return function (errors) {
+        return errors
+            .scan((acc, value) => {
+                console.log(acc, value);
+                return acc + 1;
+            }, 0)
+            .takeWhile(acc => acc < attempts)
+            .delay(delay);
+    };
 }
 
 function renderMovies(movies) {
@@ -29,11 +49,11 @@ function renderMovies(movies) {
     })
 }
 
-load('movies.json').subscribe(renderMovies);
+//load('movies.json').subscribe(renderMovies);
 
-click.flatMap(e => load('movies.json'))
-.subscribe(
+click.flatMap(e => load('moviess.json'))
+    .subscribe(
     renderMovies,
     e => console.error('error', e),
     () => console.info('complete')
-);
+    );
