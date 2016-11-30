76f3c8253992e1cf02709d524a43bfc6f0584504
Plastikaweb <info@plastikaweb.com>
2016-11-30 10:30:26 +0100

    Dealing with errors and exceptions

--- main.ts
+++ main.ts
@@ -1,45 +1,25 @@
 import { Observable } from 'rxjs/Rx';
+import { load } from './loader';
 
-let output = document.getElementById('output');
+let source = Observable.merge(
+    Observable.of(1),
+    Observable.from([2,4,5]),
+    Observable.throw(new Error('Stop!')),
+    Observable.of(8)
+).catch(e => {
+    console.log(e);
+    return Observable.of(10);
+});
+
+source.subscribe(
+    value => console.log('value', value),
+    error => console.error('error', error),
+    () => console.info('complete')
+);
+
+/*let output = document.getElementById('output');
 let btn = document.getElementsByTagName('button')[0];
 let click = Observable.fromEvent(btn, 'click');
-
-function load(url: string) {
-
-    return Observable.create(observer => {
-        let xhr = new XMLHttpRequest();
-
-        xhr.addEventListener('load', () => {
-            if (xhr.status === 200) {
-                let data = JSON.parse(xhr.responseText);
-                observer.next(data);
-                observer.complete();
-            } else {
-                observer.error(xhr.statusText);
-            }
-
-        });
-
-        xhr.open('GET', url);
-        xhr.send();
-    }).retryWhen(retryStrategy({
-        attempts: 3,
-        delay: 1500
-    }));
-
-}
-
-function retryStrategy({attempts = 4, delay = 1000}) {
-    return function (errors) {
-        return errors
-            .scan((acc, value) => {
-                console.log(acc, value);
-                return acc + 1;
-            }, 0)
-            .takeWhile(acc => acc < attempts)
-            .delay(delay);
-    };
-}
 
 function renderMovies(movies) {
     movies.forEach(m => {
@@ -51,9 +31,10 @@ function renderMovies(movies) {
 
 //load('movies.json').subscribe(renderMovies);
 
-click.flatMap(e => load('moviess.json'))
+click.flatMap(e => load('movies.json'))
     .subscribe(
     renderMovies,
     e => console.error('error', e),
     () => console.info('complete')
     );
+*/
